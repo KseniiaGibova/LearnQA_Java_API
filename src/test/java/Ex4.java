2faf1aa4b48ad216ca0dd08d45424eeb898b16e6
kseniia gibova <gibovaks@gmail.com>
2024-08-25 22:47:53 +0300

    EX3 Hello from Kseniia

---
+++ src/test/java/Ex4.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
+
+public class Ex4 {
+
+    @Test
+    public void testGET(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+
+    }
+}
