5c587a9fa5f83db4677b0551857fa2f7564f6976
kseniia gibova <gibovaks@gmail.com>
2024-09-15 00:49:49 +0300

    8 Ex вопрос

--- src/test/java/Lesson2/EX8copy.java
+++ src/test/java/Lesson2/EX8copy.java
@@ -3,10 +3,6 @@ package Lesson2;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static java.lang.Thread.sleep;
 
 public class EX8copy {
@@ -14,43 +10,49 @@ public class EX8copy {
     String token;
     int seconds;
 
-    //Map<String, String> queryParam = new HashMap<>();
    @Test
-   public void getToken() {
+   public void getToken() throws InterruptedException {
 
+       System.out.println("Создание задачи");
        JsonPath response = RestAssured
                .given()
-               .when()
                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                .jsonPath();
        response.prettyPrint();
        token = response.get("token");
        seconds = response.get("seconds");
-       System.out.println(token);}
 
-    @Test
-    public void sendRequest()
-       //первый запуск ок - данные сохранились - но как из первого теста во второй подтянуть токен?
-            //Через Hashmap и переменную не получилось, второй тест не видит сохраненную пернеменную из первого(
-            //Выводоим token на экран проверила, что после 1 теста переменная сохранилась
-            //Если запускаю 1 тест, беру токен, подставляю вручную во 2 тест - получается
+       System.out.println("Запрос до готовности задачи");
+       JsonPath response2 = RestAssured
+               .given()
+               .queryParam("token", token)
+               .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+               .jsonPath();
+       response2.prettyPrint();
+       String taskStatus = response2.get("status");
+       if (taskStatus.equals("Job is NOT ready")) {
+           System.out.println("Задача выполняется, ожидайте");
+       } else {
+           System.out.println("Ошибка выполнения задачи");
+       }
 
-               {
+       Thread.sleep(1000 * seconds);
 
-                   JsonPath response2 = RestAssured
-                    .given()
-                    .queryParam("token",token)
-                    .when()
-                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                    .jsonPath();
-            response2.prettyPrint(); }
-
-     //       if (resp == "Job is NOT ready") {
-       //         Thread.sleep(1000*seconds);
-         //   }
-
-           // response.prettyPrint();
-
+       System.out.println("Запрос по готовности задачи");
+       JsonPath response3 = RestAssured
+               .given()
+               .queryParam("token", token)
+               .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+               .jsonPath();
+       response3.prettyPrint();
+       String taskStatus3 = response3.get("status");
+       String result = response3.get("result");
+       if ((taskStatus3.equals("Job is ready")) & (!result.equals(null))) {
+           System.out.println("Задача № " + result + " выполнена успешно");
+       } else {
+           System.out.println("Ошибка выполнения задачи");
+       }
+   }
 }
 
 
